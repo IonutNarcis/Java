72054874a54c986b10493d46aadbb1bee075f200
tsakthisowmya <143686826+tsakthisowmya@users.noreply.github.com>
2024-07-02 22:37:00 +0530

    Create hcf_and_gcd.java

---
+++ Mathematics/hcf_and_gcd.java
@@ -0,0 +1,15 @@
+Factorial 
+Find the trainling zero in the factorial
+
+  int res=0
+  for(int i=5;i<=n;i*=5){
+      res = res + n/i;
+  }
+res is the answer 
+
+
+1 2 3 4 5 6 7 8 9 10 gives 38800
+it has 2 fives in it where 2 and 5 are the responsible for the 0 caused in the end 
+hence we use this method to fing the trialing zero
+
+
